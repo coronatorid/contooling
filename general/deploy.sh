ebcfed799c83d3ff11cebe047aea576b05a1b1f8
insomnius <aliana.arib@gmail.com>
2020-12-25 06:00:50 +0700

    Minor fix

--- general/deploy.sh
+++ general/deploy.sh
@@ -7,11 +7,12 @@
 # SSH_PUBLIC_KEY
 # SERVER_USER
 # SERVER_HOST
+# USERPASSWORD
 
 
 echo "START DEPLOYMENT PROCESS"
 
-if [ -z $SERVICE ] || [ -z $RELEASE_TAG ] || [ -z $SSH_PRIVATE_KEY ] || [ -z $SSH_PUBLIC_KEY ] || [ -z $SERVER_USER ] || [ -z $SERVER_HOST ]; then
+if [ -z $SERVICE ] || [ -z $RELEASE_TAG ] || [ -z $SSH_PRIVATE_KEY ] || [ -z $SSH_PUBLIC_KEY ] || [ -z $SERVER_USER ] || [ -z $SERVER_HOST ] || [ -z $USERPASSWORD ]; then
     echo "Some variable is not initiated, exiting deployment process."
     exit 1
 fi
@@ -37,8 +38,23 @@ if [ -d "./$SERVICE" ]; then
     
     echo "Copying files into server"
     
+    echo "
+        docker-compose up
+    " >> ./runner.sh
+    
+    echo "
+[program:$SERVICE]
+command=/opt/$SERVICE/runner.sh
+autostart=true
+autorestart=true
+stderr_logfile=/var/log/$SERVICE.err.log
+stdout_logfile=/var/log/$SERVICE.out.log
+    " >> ./supervisor.conf
+    
     ssh -f ./id_rsa.pub $SERVER_USER@$SERVER_HOST "mkdir -p /opt/$SERVICE/"
     scp -f ./id_rsa.pub ./... $SERVER_USER@$SERVER_HOST:/opt/$SERVICE/
+    scp -f ./id_rsa.pub ./supervisor.conf $SERVER_USER@$SERVER_HOST:/etc/supervisor/conf.d/$SERVICE.conf
+    ssh -f ./id_rsa.pub $SERVER_USER@$SERVER_HOST "echo $USERPASSWORD | sudo -S service supervisor restart"
 else
     echo "Error: directory $SERVICE not found. Can not continue."
     exit 1
