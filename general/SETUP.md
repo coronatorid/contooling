835d65331e021d366bc9c002f47cecf93cb1e3f0
insomnius <aliana.arib@gmail.com>
2020-12-25 02:15:02 +0700

    Create setup.sh

--- general/SETUP.md
+++ general/SETUP.md
@@ -1,3 +1,13 @@
 # Setup
 
 This is installation guide for every instance that want to build server for coronator.
+
+## Step
+
+### Run setup script
+
+Setup script is needed if you are deploying new compute instances.
+
+```
+sh setup.sh
+```
---
+++ general/setup.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+echo "Preparing instance for coronator infrastructure"
+echo "==============================================="
+
+cd
+
+echo "Update repository"
+echo "==============================================="
+
+sudo apt update
+sudo apt upgrade
+
+
+
+
+echo "Installing prerequisites"
+echo "==============================================="
+
+sudo apt install apt-transport-https
+sudo apt install ca-certificates
+sudo apt install curl
+sudo apt install gnupg-agent
+sudo apt install software-properties-common
+
+
+
+
+echo "Installing docker"
+echo "==============================================="
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io
+
+echo "Docker version: $(docker --version)"
+echo "-----------------------------------------------"
+
+
+
+echo "Installing docker-compose"
+echo "==============================================="
+
+sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+
+echo "Docker compose version: $(docker-compose --version)"
+echo "-----------------------------------------------"
+
+
+
+echo "Installing supervisor"
+echo "==============================================="
+sudo apt-get install supervisor
+service supervisor restart
