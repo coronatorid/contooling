c1bba2f68a21931162288a7acbde511c57d1aee4
insomnius <aliana.arib@gmail.com>
2020-12-25 03:26:21 +0700

    scp ssh

--- general/deploy.sh
+++ general/deploy.sh
@@ -3,22 +3,43 @@
 # Variable
 # SERVICE = deployed services
 # RELEASE_TAG = deployment tag of a services
+# SSH_PRIVATE_KEY
+# SSH_PUBLIC_KEY
+# SERVER_USER
+# SERVER_HOST
+
 
 echo "START DEPLOYMENT PROCESS"
 
-if [ -d "../$SERVICE" ]; then
-  echo "Deploying $SERVICE"
-  echo "Change directory to respective services"
+if [ -z $SERVICE ] || [ -z $RELEASE_TAG ] || [ -z $SSH_PRIVATE_KEY ] || [ -z $SSH_PUBLIC_KEY ] || [ -z $SERVER_USER ] || [ -z $SERVER_HOST ]; then
+    echo "Some variable is not initiated, exiting deployment process."
+    exit 1
+fi
 
-  cd ../$SERVICE
-
-  echo "Showing all files in directory"
-  echo "========================================"
-  ls -la
-  echo "========================================"
-
-  echo "TAG=$RELEASE_TAG" >> .env
+if [ -d "./$SERVICE" ]; then
+    echo "Deploying $SERVICE. Release: $RELEASE_TAG"
+    echo "Change directory to respective services"
+    
+    cd ./$SERVICE
+    
+    echo "Showing all files in directory"
+    echo "========================================"
+    ls -la
+    echo "========================================"
+    
+    echo "TAG=$RELEASE_TAG" >> .env
+    
+    echo "========================================"
+    echo "SSH Preparation"
+    
+    echo "$SSH_PRIVATE_KEY" >> ./id_rsa
+    echo "$SSH_PUBLIC_KEY" >> ./id_rsa.pub
+    
+    echo "Copying files into server"
+    
+    ssh -f ./id_rsa.pub $SERVER_USER@$SERVER_HOST "mkdir -p /opt/$SERVICE/"
+    scp -f ./id_rsa.pub ./... $SERVER_USER@$SERVER_HOST:/opt/$SERVICE/
 else
-  echo "Error: directory $SERVICE not found. Can not continue."
-  exit 1
+    echo "Error: directory $SERVICE not found. Can not continue."
+    exit 1
 fi
